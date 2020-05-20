68b0a55e3a0596c429d123a0be186c0538fa3749
anasabdulwahed <anasabdelwahed0113@gmail.com>
2020-05-21 02:40:51 +0300

    project calculator

---
+++ project calculator/script.js
@@ -0,0 +1,88 @@
+var  inp   = document.getElementById("myinput").value,
+     btn1  = document.getElementById("btn1").value,
+     btn2  = document.getElementById("btn2").value,
+     btn3  = document.getElementById("btn3").value,
+     btn4  = document.getElementById("btn4").value,
+     btn5  = document.getElementById("btn5").value,
+     btn6  = document.getElementById("btn6").value,
+     btn7  = document.getElementById("btn7").value,
+     btn8  = document.getElementById("btn8").value,
+     btn9  = document.getElementById("btn9").value,
+     btn10 = document.getElementById("btn10").value,
+     btn11 = document.getElementById("btn11").value,
+     btn12 = document.getElementById("btn12").value,
+     btn13 = document.getElementById("btn13").value,
+     btn14 = document.getElementById("btn14").value,
+     btn15 = document.getElementById("btn15").value,
+     btn16 = document.getElementById("btn16").value;
+      
+    
+function concatone(){
+  inp += btn1;
+    document.getElementById("myinput").value = inp;  
+}
+function concattwo(){
+     inp += btn2;
+    document.getElementById("myinput").value = inp;  
+}
+function concatthree(){
+    inp += btn3;
+    document.getElementById("myinput").value = inp;  
+}
+function concatplus(){
+     inp += btn4;
+    document.getElementById("myinput").value = inp;  
+}
+function concatfour(){
+     inp += btn5;
+    document.getElementById("myinput").value = inp;  
+}
+function concatfive(){
+     inp += btn6;
+    document.getElementById("myinput").value = inp;  
+}
+function concatsix(){
+     inp += btn7;
+    document.getElementById("myinput").value = inp;  
+}
+function concatminus(){
+     inp += btn8;
+    document.getElementById("myinput").value = inp;  
+}
+function concatseven(){
+     inp += btn9;
+    document.getElementById("myinput").value = inp;  
+}
+function concategit(){
+     inp += btn10;
+    document.getElementById("myinput").value = inp;  
+}
+function concatnine(){
+     inp += btn11;
+    document.getElementById("myinput").value = inp;  
+}
+function concatmul(){
+     inp += btn12;
+    document.getElementById("myinput").value = inp;  
+}
+function concatzero(){
+     inp += btn13;
+    document.getElementById("myinput").value = inp;  
+}
+function concatrest(){
+     inp = "";
+    document.getElementById("myinput").value = inp;  
+}
+function concategual(){
+      document.getElementById("myinput").value = //The eval() function evaluates JavaScript code represented as a string
+ eval(inp);
+}
+
+function concatdiv(){
+   inp += btn16;
+    document.getElementById("myinput").value = inp;  
+}
+
+
+
+
